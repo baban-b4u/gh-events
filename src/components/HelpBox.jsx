ed7a69118a3410e3e9868766da0d5db07f19885c
Baban Basarikatti <baban.basarikatti@epicor.com>
2026-06-16 22:50:05 +0530

    cancel workflow

--- src/components/HelpBox.jsx
+++ src/components/HelpBox.jsx
@@ -2,6 +2,7 @@ import { string } from 'prop-types';
 
 import './HelpBox.css';
 // added comments
+// additional comments
 function HelpBox({ title, text }) {
   return (
     <article className="help-box">
